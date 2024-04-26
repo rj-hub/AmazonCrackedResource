5f3b2547c561bf6c01e66d54762e541805fc8229
rj-hub <52329760+rj-hub@users.noreply.github.com>
2024-04-26 23:15:25 +0530

    Update week1_number_of_island.java
    7ms
    Beats
    13.90%
    of users with Java

--- week1_number_of_island.java
+++ week1_number_of_island.java
@@ -1,1 +1,31 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+        for(int i = 0 ; i < rows;  i++){
+            for(int j = 0; j < cols; j++) {
+                if(Objects.equals(grid[i][j],'1')) {
+                    count++;
+                    dpf(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
 
+    void dpf(char[][] grid, int x, int y) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if(x<0 || x>=rows || y<0 || y>=cols || Objects.equals(grid[x][y],'0'))
+            return;
+        
+        grid[x][y] = '0';
+
+        dpf(grid,x+1,y);
+        dpf(grid,x-1,y);
+        dpf(grid,x,y+1);
+        dpf(grid,x,y-1);
+    }
+}
